8e48d7962782db69b3d07233a0feaad47d2ee4cc
gamenite00 <brendanmfisher@gmail.com>
2024-06-29 17:58:28 -0600

    Initial Project Files

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour{
+    
+    public Rigidbody rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+    }
+}
